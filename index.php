4013d359a2833bbbdcd1631aceca7e2058a4a745
Jonatas Maximiano <maximianojonatas@gmail.com>
2015-02-25 10:04:43 -0300

    Troquei para o meu nome

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE HTML>
-<?php
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Exibindo nome PHP </title>
+    </head>
+    <body>
+        <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+        /*
+         * To change this template, choose Tools | Templates
+         * and open the template in the editor.
+         */
 
-  echo 12314;
-  echo "<h1 align='center'> Jonatas </h1>";
-?>
+          echo 12314;
+          echo "<h1 align='center'> Jonatas </h1>";
+        ?>
+    </body>
+</html>
