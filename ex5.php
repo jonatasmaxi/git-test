30479e9f5a47202e16e5fa751510fde5045184e8
41404998 <41404998@090017-P33S13.macklabs.br>
2015-03-04 10:33:39 -0300

    Inclusão do exercício 5

---
+++ ex5.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<HTML>
+    <HEAD>
+        <TITLE> Exercício 5 </TITLE>
+        <meta charset="UTF-8">
+    </HEAD>
+    <body>
+        <table>
+            <thead>
+                <td> Turma </td>
+                <td> Disciplina </td>
+                <td> Semana </td>
+                <td> Professor </td>
+            </thead>
+            <?php 
+              $horario[0][0] = '02K01';
+              $horario[0][1] = 'Tecnologia Web II';
+              $horario[0][2] = 'Segunda';
+              $horario[0][3] = 'Charles';
+              $horario[1][0] = '02J01';
+              $horario[1][1] = 'Linguagem de Programação I';
+              $horario[1][2] = 'Terça';
+              $horario[1][3] = 'João';
+              $horario[2][0] = '02K01';
+              $horario[2][1] = 'Linguagem De Programação I';
+              $horario[2][2] = 'Quarta';
+              $horario[2][3] = 'Charles';
+            ?>
+        </table>
+    </body>
+</HTML>
